f1998d15a45db922c33e87acd114ca7acd60f7e9
PigYoung90 <PigYoung90@DESKTOP-JV4EDD1>
2023-09-27 12:39:14 +0900

    # WARNING: head commit changed in the meantime
    test commit 잔디가 안심어진다!?

--- src/M2023_9/D0927.java
+++ src/M2023_9/D0927.java
@@ -30,11 +30,14 @@ public void setSecondPassword(String secondPassword) {
 // DTO의 경우 비즈니스 로직 단계 등에서 값이 변경 될 수 있기 때문에 set이 들어가 있음.
 /* 
  * 접근제한자.
- * public - 모든 클래스 및 패키지 등에서 사용이 가능
+ * public - 모든 클래스 및 패키지 등에서 사용이 가능 -> JVM이 접근하기 위해서도 public을 사용해야함.
  * default - 해당 클래스와 패키지까지 사용이 가능  
  * protected -해당 클래스와 패키지 그리고 상속된 클래스에서 사용가능
  * private -해당 클래스에서만 사용이 가능
  * 
+ * void -  리턴타입이 없음. 문법적인 이유라고 보면됨. 해당 void가 없을경우 return이 있어야함.
+ * return - void가 아닌경우 사용하는데, 해당 return (내용) ; 해주면 해당 내용을 리턴함 .
+ * -? 어디로 리턴함!?
  */
 	
 }
