3e17cebd1c15cd29299d75de781d5efc1808683c
YoungHyoKim90 <younghyo1101@gmail.com>
2023-10-04 16:10:05 +0900

    while exam

--- src/M2023_10/D_1004.java
+++ src/M2023_10/D_1004.java
@@ -1,5 +1,19 @@
 package M2023_10;
 
 public class D_1004 {
-
+/*
+ *  while (조건) { 반복 실행 영역 }
+ *  조건 -> 언제까지 반복할 것이냐.참이면 실행 거짓이면 정지.
+ *  
+ */
+	
+	public static void main(String[] args) {
+		int cnt = 1;
+		while(true)	{
+			System.out.println("반복문입니다.");
+			System.out.println(cnt + "번 반복중 입니다.");
+			cnt ++;
+			System.out.println("---------------------");
+		}
+	}
 }
