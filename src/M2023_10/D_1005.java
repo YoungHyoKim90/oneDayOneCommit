ac92bc74431809cc6854e9766b99df7f0f3f8994
YoungHyoKim90 <younghyo1101@gmail.com>
2023-10-05 16:42:25 +0900

    23.10.05 commit

---
+++ src/M2023_10/D_1005.java
@@ -0,0 +1,84 @@
+package M2023_10;
+
+public class D_1005 {
+
+	/*
+	 * 
+	 
+	 <!DOCTYPE html>
+<html>
+<head>
+    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+</head>
+<body>
+
+    <span id="labelAll">전체<input  id="allCk" type="checkbox"></input></span>
+    <span id="label1">테스트1<input id="ck1" type="checkbox"></input></span>
+    <span id="label2">테스트2<input id="ck2" type="checkbox"></input></span>
+    <span id="label3">테스트3<input id="ck3" type="checkbox"></input></span>
+    <br>
+    <button type="button" id="btn1">1번 체크/언체크</button>
+    <button type="button" id="btn2">2번 체크/언체크</button>
+    <button type="button" id="btn3">3번 체크/언체크</button>
+</body>
+    
+<script>
+    $('#btn1').click(function(){
+        var checked = $('#ck1').is(':checked');
+        $('#ck1').prop('checked',!checked);
+    });
+    
+    $('#btn2').click(function(){
+        var checked = $('#ck2').is(':checked');
+        $('#ck2').prop('checked',!checked);
+    });
+    
+    $('#btn3').click(function(){
+        var checked = $('#ck3').is(':checked');
+        $('#ck3').prop('checked',!checked);
+    });
+    
+    $('#allCk').click(function(){
+        var checked = $('#allCk').is(':checked');
+        $('input:checkbox').prop('checked',!checked);
+    });
+    $('#labelAll').click(function(){
+        var checked = $('#allCk,#ck1,#ck2,#ck3').is(':checked');
+        $('input:checkbox').prop('checked',!checked);
+    });
+
+    $('#label1').click(function(){
+        var checked = $('#ck1').is(':checked');
+        $('#ck1').prop('checked',!checked);
+    });
+
+    $('#label2').click(function(){
+        var checked = $('#ck2').is(':checked');
+        $('#ck2').prop('checked',!checked);
+    });
+
+    $('#label3').click(function(){
+        var checked = $('#ck3').is(':checked');
+        $('#ck3').prop('checked',!checked);
+    });
+
+   $('#ck1').click(function(){
+    var checked =$('#ck1').is(':checked');
+    $('#ck1').prop('checked',!checked);
+   });
+   $('#ck2').click(function(){
+    var checked =$('#ck2').is(':checked');
+    $('#ck2').prop('checked',!checked);
+   });
+   $('#ck3').click(function(){
+    var checked =$('#ck3').is(':checked');
+    $('#ck3').prop('checked',!checked);
+   });
+</script>
+</html>
+
+	 
+	 * 
+	 */
+	
+}
