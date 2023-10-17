aa6cb5de196c1a81265774d7e8530c2f9fb112ca
YoungHyoKim90 <younghyo1101@gmail.com>
2023-10-17 16:59:13 +0900

    2023.10.17  Jquery 코드

---
+++ src/M2023_10/D_1017_2.java
@@ -0,0 +1,33 @@
+package M2023_10;
+
+public class D_1017_2 {
+
+	/* 버튼을 누르면 안녕하세요를 alert 로 보여주는 코드.
+	 * 
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	    <meta charset="UTF-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	    <title>Document</title>
+	    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+	</head>
+	</head>
+	<body>
+	    <button id="b1" class="btn">Button1</button>
+	    <button id="b2" class="btn">Button2</button>
+	    <button id="b3" class="btn">Button3</button>
+	    <button id="b4" class="btn">Button4</button>
+	
+	    <script>
+	        $('.btn').on('click',function(){
+	            alert('안녕하세요.')
+	        });
+	    </script>
+	
+	</body>
+	</html>
+	 *
+	 */
+	
+}
