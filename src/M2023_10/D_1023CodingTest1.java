e52cc50ff71745424637f559fc33b5cf33276ac7
YoungHyoKim90 <younghyo1101@gmail.com>
2023-10-23 09:47:49 +0900

    2023.10.23 commit(2)

---
+++ src/M2023_10/D_1023CodingTest1.java
@@ -0,0 +1,28 @@
+package M2023_10;
+
+import java.util.Scanner;
+
+public class D_1023CodingTest1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("입력해주세요.");
+		
+		try {
+			String str = sc.next();
+			int n = sc.nextInt();
+			System.out.println("입력받은 값은 = " + str + "입니다." );
+			if(str.length() <= 10 && str != null && str != "") {
+				for(int i=0; i < str.length(); i++) {
+					System.out.println((i+ 1) + "번째 문자는" + str.charAt(i) + "입니다.");
+				}
+			System.out.println("끝");
+			
+		} 
+		}catch (Exception e) {
+			System.out.println("숫자를 입력해 주세요.");
+			
+		}
+	}
+	
+}
---
+++ src/M2023_10/D_1023_CodeTest01.java
@@ -0,0 +1,23 @@
+package M2023_10;
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+public class D_1023_CodeTest01 {
+
+	int solution(int a, int b) {
+	    int answer = 0;
+	    if (a % 2 == 1 && b % 2 == 1) {
+	        answer = a * a + b * b;
+	    }
+	    // a와 b 중 하나만 홀수라면 2 * (a + b) 점을 얻습니다.
+	    else if (a % 2 == 1 || b % 2 == 1) {
+	        answer = 2 * (a + b);
+	    }
+	    // a와 b 모두 홀수가 아니라면 |a - b| 점을 얻습니다.
+	    else {
+	        answer = abs(a - b);
+	    }
+	    return answer;
+	}
+}
