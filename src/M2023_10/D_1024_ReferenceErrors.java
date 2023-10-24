f3c1c1018ffa6c6b6d66b5aa877d3d736d477d18
YoungHyoKim90 <younghyo1101@gmail.com>
2023-10-24 09:58:05 +0900

    23.10.24 (1) commit 레퍼런스 에러

---
+++ src/M2023_10/D_1024_ReferenceErrors.java
@@ -0,0 +1,27 @@
+package M2023_10;
+
+public class D_1024_ReferenceErrors {
+
+	// JS Code
+	
+	/* 레퍼런스 찹기
+	 * try {
+		  let a = undefinedVariable;
+		} catch (e) {
+		  console.log(e instanceof ReferenceError); // true
+		  console.log(e.message); // "undefinedVariable is not defined"
+		  console.log(e.name); // "ReferenceError"
+		  console.log(e.stack); // Stack of the error
+		}
+		// 레퍼런스 생성
+		try {
+		  throw new ReferenceError("Hello");
+		} catch (e) {
+		  console.log(e instanceof ReferenceError); // true
+		  console.log(e.message); // "Hello"
+		  console.log(e.name); // "ReferenceError"
+		  console.log(e.stack); // Stack of the error
+		}
+	 */
+	
+}
