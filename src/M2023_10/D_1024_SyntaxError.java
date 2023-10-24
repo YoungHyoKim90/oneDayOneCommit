a0783834d4d8c7732f221672e0b40db30d30368e
YoungHyoKim90 <younghyo1101@gmail.com>
2023-10-24 11:13:13 +0900

    23.10.24 SyntaxError Commit

---
+++ src/M2023_10/D_1024_SyntaxError.java
@@ -0,0 +1,27 @@
+package M2023_10;
+
+public class D_1024_SyntaxError {
+
+	//JS Code
+	
+	/*SyntaxError 처리하기
+	 * try {
+		  eval("hoo bar");
+		} catch (e) {
+		  console.log(e instanceof SyntaxError); // true
+		  console.log(e.message);
+		  console.log(e.name); // "SyntaxError"
+		  console.log(e.stack); // 오류 스택
+		}
+		SyntaxError 생성하기
+		try {
+		  throw new SyntaxError("Hello");
+		} catch (e) {
+		  console.log(e instanceof SyntaxError); // true
+		  console.log(e.message); // "Hello"
+		  console.log(e.name); // "SyntaxError"
+		  console.log(e.stack); // 오류 스택
+		}
+	 */
+	
+}
