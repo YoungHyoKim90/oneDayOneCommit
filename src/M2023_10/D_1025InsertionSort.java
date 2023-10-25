91953e27a2c50efee2ad52a0bb6afed4ec19c2c0
YoungHyoKim90 <younghyo1101@gmail.com>
2023-10-25 20:08:41 +0900

    23.10.25(4) Insertion Sort

---
+++ src/M2023_10/D_1025InsertionSort.java
@@ -0,0 +1,62 @@
+package M2023_10;
+
+import java.util.Scanner;
+
+/*
+ * Insertion Sort
+	개요
+	삽입 정렬은 자료 배열의 모든 요소를 앞에서부터 차례대로 이미 정렬된 배열 부분과 비교하여,
+	자신의 위치를 찾아 삽입함으로써 정렬을 완성하는 알고리즘이다.
+	예시문제
+	주어진 데이터를 Insertion Sort를 사용하여 정렬 하시오.
+	데이터의 최대 크기는 100이다.
+ */
+public class D_1025InsertionSort {
+
+	static int input[];
+	static int num;
+
+	static void insertionSort() {
+		for (int i = 1; i < num; i++) {
+			int temp = input[i];
+			int j = i - 1;
+
+			while ((j >= 0) && (temp < input[j])) {
+				input[j + 1] = input[j];
+				j = j - 1;
+			}
+			input[j + 1] = temp;
+		}
+	}
+
+	static void printResult() {
+		int i;
+
+		for (i = 0; i < num; ++i) {
+			System.out.print(input[i] + " ");
+		}
+		System.out.println();
+	}
+
+	public static void main(String arg[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+
+		for (int test_case = 1; test_case <= T; test_case++) {
+			num = sc.nextInt();
+
+			input = new int[num];
+			for (int i = 0; i < num; i++) {
+				input[i] = sc.nextInt();
+			}
+
+			insertionSort();
+
+			System.out.print("#" + test_case + " ");
+			printResult();
+		}
+
+		sc.close();
+	}
+}
--- src/M2023_10/D_1025RecursionFunction.java
+++ src/M2023_10/D_1025RecursionFunction.java
@@ -14,26 +14,41 @@ Recursion
 public class D_1025RecursionFunction {
 
 	static long factorial(int num) {
+		int count =1;
 		if (num == 0) {
+			count++;
+			System.out.println("factorial if문 실행중 입니다.");
+			System.out.println(count + "번 실행되었습니다.");
 			return 1;
 		} else {
+			count ++ ;
+			System.out.println("factorial else문 실행중입니다.");
+			System.out.println(count + "번 실행되었습니다.");
 			return num * factorial(num - 1);
 		}
 	}
 
 	public static void main(String arg[]) throws Exception {
+		int count =1;
 		Scanner sc = new Scanner(System.in);
-
+		System.out.println("Scanner실행되었습니다. 원하는 T의 숫자를 입력해 주세요.");
 		int T = sc.nextInt();
+		System.out.println(T+"를 입력받았습니다.");
 
 		for (int test_case = 1; test_case <= T; ++test_case) {
+			count ++;
+			System.out.println("main의 for문이  " + count + "번 실행되었습니다.");
+			System.out.println("num값을 입력해주세요.");
 			int num = sc.nextInt();
-
+			System.out.println(num + "을 입력받았습니다.");
+			System.out.println("-------------------");
+			System.out.println("num 값은 : " + num + " T 값은 : " + T);
 			long value = factorial(num);
+			System.out.println("long value = factorial(" + num + "); 를 실행하였습니다.");
 
 			System.out.println("#" + test_case + " " + num + "! = " + value);
 		}
-
+		System.out.println("해당 프로그램을 종료합니다.");
 		sc.close();
 	}
 }
