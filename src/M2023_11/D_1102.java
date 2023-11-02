4e860f14ff74ec64cce2883a6f292e2215a84c2d
YoungHyoKim90 <younghyo1101@gmail.com>
2023-11-02 18:34:02 +0900

    23.11.02 commit

---
+++ src/M2023_11/D_1102.java
@@ -0,0 +1,23 @@
+package M2023_11;
+
+public class D_1102 {
+/*
+ * Jquery(document).ready(function(){
+ * try{
+ * if(autosuggerstUserModify){
+ * $".token-input").addClass("modify");
+ * }
+ * if(1isMultiAutosuggest){
+ * tokeninput.ser("hwto","hwcc",hwbcc");
+ * tokenInput.autoSuggestyInit({
+ * userId : '00380100',
+ * contact :'true'
+ * }_;
+ * initTokenInput();
+ * sortsuggest.init();
+ * tokenInput.callback(function(toList,ccList,bccList){
+ * if(window.console){
+ * console.log("클리어 테스트 스타트");
+ * };
+ */
+}
