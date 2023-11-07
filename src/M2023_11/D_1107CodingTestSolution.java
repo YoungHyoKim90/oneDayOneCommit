43f2856da9edf087274f5a5fd657f852a2b68672
YoungHyoKim90 <younghyo1101@gmail.com>
2023-11-07 09:10:22 +0900

    23.11.07 CodingTest Commit

---
+++ src/M2023_11/D_1107CodingTestSolution.java
@@ -0,0 +1,26 @@
+package M2023_11;
+
+import java.util.Scanner;
+
+public class D_1107CodingTestSolution {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		int c = a + b;
+
+		System.out.println(a + " + " + b + " = " + c);
+		System.out.println(a + " + " + b + " = " + (a + b));
+		
+		int sum = a + b;
+
+        System.out.println(a + " + " + b + " = " + sum);
+        
+        String c = String.format("%d + %d = %d", a, b, a+b);
+        System.out.println(c);
+        System.out.println(String.format("%d + %d = %d", a, b, a+b));
+	}
+
+}
