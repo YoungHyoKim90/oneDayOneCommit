bce030c1164bd560f990a2d5dddc39015a11c443
YoungHyoKim90 <younghyo1101@gmail.com>
2023-11-14 09:57:20 +0900

    23.11.14(2)

--- src/M2023_11/D_1114RockyLinuxText.java
+++ src/M2023_11/D_1114RockyLinuxText.java
@@ -1,5 +1,15 @@
 package M2023_11;
 
 public class D_1114RockyLinuxText{
+	
+/*
+ * rocky Linux는 Red hat Enterprise Linux(RHEL)의 클론 버전으로 Red Hat Enterprise Linux의 소스를 그대로 가져다가 재컴파일한 것이다.
+ * RHEL 8까지는 클론 버전으로 CentOS가 대세였으나, RHEL 9 부터는 Rocky Linux가 대세이다. 
+ * Rocky Linux는 centOS 8 의 기능을 100% 사용할 수 있을 뿐아니라, 무료로 사용이 가능하다.
+ * 
+ * VMware 가상머신 : 물리적인 컴퓨터 1대에서 여러대의 컴퓨터로 운영하는 것과 같은 역활을 하는 소프트웨어
+ * 
+ * 라우터(게이트웨이) : 2개 이상의 네트워크를 연결하는 소프트웨어 혹은 컴퓨터, 대부분 LAN <-> WAN 으로 전환.
+ */
 
 }
