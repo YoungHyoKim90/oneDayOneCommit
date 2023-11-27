6ccc1639d7cb1fd3748cb34cdf7f4ef2697920f9
YoungHyoKim90 <younghyo1101@gmail.com>
2023-11-27 10:53:24 +0900

    경마게임

--- src/M2023_11/HorseRaceGame.java
+++ src/M2023_11/HorseRaceGame.java
@@ -1,6 +1,7 @@
 package M2023_11;
-import java.util.Scanner;
+import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class HorseRaceGame {
     public static void main(String[] args) {
@@ -19,25 +20,56 @@ public class HorseRaceGame {
             // 각 말에 대해 이동 거리를 랜덤하게 설정
             for (int i = 0; i < horses.length; i++) {
                 int distance = random.nextInt(3) + 1; // 1부터 3까지의 랜덤한 값으로 이동 거리 설정
+
+                // 말이 3칸을 이동할 때 특정 메시지 출력
+                if (distance == 3) {
+                    System.out.println((i + 1) + "번 말이 속도를 내네요!");
+                }
+
+                // 말의 위치가 비슷하게 이동할 때 메시지 출력
+                for (int j = 0; j < horses.length; j++) {
+                    if (i != j && horses[i] == horses[j]) {
+                        System.out.println((i + 1) + "번 말이 " + (j + 1) + "번 말과 비슷한 위치에 있습니다!");
+                    }
+                }
+
                 horses[i] += distance;
             }
 
             // 말의 현재 위치 출력
             displayHorsePositions(horses);
 
-            // 사용자가 다음 단계를 진행할 준비가 되었다는 메시지 출력
-            System.out.println("Enter를 눌러 다음 단계로 진행하세요.");
-            scanner.nextLine(); // 사용자 입력 대기
+            // 일시정지를 위해 대기
+            try {
+                Thread.sleep(1000); // 1초 대기
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
         // 경주가 끝났음을 알리는 메시지 출력
         System.out.println("경주가 종료되었습니다!");
+
+        // 경주 결과 출력
+        displayRaceResult(horses);
+
         scanner.close();
     }
 
     // 말들의 현재 위치를 출력하는 함수
     private static void displayHorsePositions(int[] horses) {
-        // 여기에 말들의 현재 위치를 출력하는 코드를 작성하세요
+        int trackLength = 20; // 경주 트랙 길이
+        for (int i = 0; i < horses.length; i++) {
+            System.out.print(i + 1 + "번 말: ");
+            for (int j = 0; j < trackLength; j++) {
+                if (horses[i] == j) {
+                    System.out.print("0");
+                } else {
+                    System.out.print("-");
+                }
+            }
+            System.out.println();
+        }
     }
 
     // 경주가 끝났는지 확인하는 함수
@@ -51,4 +83,27 @@ public class HorseRaceGame {
         return false; // 모든 말이 최대 거리에 도달하지 않았으면 false 반환
     }
 
+    private static void displayRaceResult(int[] horses) {
+        System.out.println("경주가 종료되었습니다! 결과는 다음과 같습니다:");
+
+        // 각 말의 최종 위치를 출력하여 순위를 표시합니다.
+        int[] sortedHorses = Arrays.copyOf(horses, horses.length);
+        Arrays.sort(sortedHorses);
+
+        for (int i = sortedHorses.length - 1; i >= 0; i--) {
+            int horseIndex = findHorseIndex(horses, sortedHorses[i]) + 1;
+            System.out.println(horseIndex + "번 말이 " + (sortedHorses.length - i) + "등에 위치했습니다.");
+        }
+    }
+
+    private static int findHorseIndex(int[] horses, int distance) {
+        for (int i = 0; i < horses.length; i++) {
+            if (horses[i] == distance) {
+                return i;
+            }
+        }
+        return -1; // 발견되지 않으면 -1 반환
+    }
 }
+
+
