9abd5e17c78f1d93f03f5cd13d382b2a536ffeff
YoungHyoKim90 <younghyo1101@gmail.com>
2023-11-30 10:24:49 +0900

    반복문

---
+++ src/M2023_11/ForEachLoop.java
@@ -0,0 +1,12 @@
+package M2023_11;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        // for-each 반복문을 사용하여 배열의 요소 출력
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+    }
+}
