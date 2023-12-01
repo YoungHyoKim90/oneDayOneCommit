14596c4f0af7ac71b6ca0f73b60962be7c050c65
YoungHyoKim90 <younghyo1101@gmail.com>
2023-12-01 10:43:20 +0900

    고양이와 멍멍이

---
+++ src/M2023_11/AnimalExample.java
@@ -0,0 +1,32 @@
+package M2023_11;
+
+//동물 인터페이스
+interface Animal {
+ void makeSound();
+}
+
+//구체적인 동물 클래스 - 고양이
+class Cat implements Animal {
+ @Override
+ public void makeSound() {
+     System.out.println("야옹");
+ }
+}
+
+//구체적인 동물 클래스 - 강아지
+class Dog implements Animal {
+ @Override
+ public void makeSound() {
+     System.out.println("멍멍");
+ }
+}
+
+public class AnimalExample {
+ public static void main(String[] args) {
+     Animal cat = new Cat();
+     Animal dog = new Dog();
+
+     cat.makeSound(); // "야옹" 출력
+     dog.makeSound(); // "멍멍" 출력
+ }
+}
