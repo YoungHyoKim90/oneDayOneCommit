264d2ae2f3faff3f554d753d938f41596d094ae0
YoungHyoKim90 <younghyo1101@gmail.com>
2023-12-04 13:04:47 +0900

    언어찾기

---
+++ src/M2023_12/LanguageDetection.java
@@ -0,0 +1,27 @@
+package M2023_12;
+
+//import org.apache.tika.language.detect.LanguageDetector;
+//import org.apache.tika.language.detect.LanguageDetectorFactory;
+//import org.apache.tika.language.detect.LanguageResult;
+//
+//public class LanguageDetection {
+//    public static void main(String[] args) {
+//        String text = "여기에 텍스트 입력"; // 분석할 텍스트 입력
+//
+//        // Apache Tika의 LanguageDetector 사용
+//        LanguageDetector detector = LanguageDetectorFactory.create();
+//
+//        LanguageResult result = detector.detect(text);
+//
+//        if (result != null) {
+//            String detectedLanguage = result.getLanguage();
+//            double confidence = result.getRawScore();
+//
+//            System.out.println("입력된 텍스트의 언어: " + detectedLanguage);
+//            System.out.println("신뢰도: " + confidence);
+//        } else {
+//            System.out.println("언어를 감지할 수 없습니다.");
+//        }
+//    }
+//}
+
