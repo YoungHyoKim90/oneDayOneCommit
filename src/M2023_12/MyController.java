a54fcfb4128acda2f5bc807e1c16a1276e17f57e
YoungHyoKim90 <younghyo1101@gmail.com>
2023-12-14 00:04:20 +0900

    MyController

---
+++ src/M2023_12/MyController.java
@@ -0,0 +1,11 @@
+package M2023_12;
+
+//@Controller
+//public class MyController {
+//    @GetMapping("/myEndpoint")
+//    public String myMethod(HttpSession session) {
+//        Object value = session.getAttribute("attributeName"); // 세션 속성 확인
+//        return "myView";
+//    }
+//}
+
