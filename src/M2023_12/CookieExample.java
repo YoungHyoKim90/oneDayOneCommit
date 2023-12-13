72ce0136cdc12918dfb9e3b4694dfbc2e2b23ecb
YoungHyoKim90 <younghyo1101@gmail.com>
2023-12-13 23:15:05 +0900

    CookieExample

---
+++ src/M2023_12/CookieExample.java
@@ -0,0 +1,26 @@
+package M2023_12;
+
+//import javax.servlet.http.Cookie;
+//
+//public class CookieExample {
+//    public static void main(String[] args) {
+//        // 예시로 쿠키 생성
+//        Cookie cookie = new Cookie("sessionId", "abc123");
+//
+//        // Secure 플래그 설정 (HTTPS 연결에서만 전송)
+//        cookie.setSecure(true);
+//
+//        // HttpOnly 플래그 설정 (JavaScript에서 접근 불가)
+//        cookie.setHttpOnly(true);
+//
+//        // SameSite 플래그 설정 (SameSite=None; Secure와 함께 사용할 경우 HTTPS에서만 전송)
+//        cookie.setSameSite(Cookie.SameSite.NONE);
+//
+//        // 쿠키 유효 기간 설정 (예시로 1시간)
+//        cookie.setMaxAge(60 * 60); // 초 단위
+//
+//        // 쿠키를 응답 헤더에 추가하여 클라이언트에 전송
+//        // response.addCookie(cookie);
+//    }
+//}
+
