043db2b198f668c4f47e8c4e7de2e34e07baeb68
YoungHyoKim90 <younghyo1101@gmail.com>
2023-12-18 00:14:37 +0900

    BasicJavaExample

---
+++ src/M2023_12/BasicJavaExample.java
@@ -0,0 +1,23 @@
+package M2023_12;
+
+public class BasicJavaExample {
+    public static void main(String[] args) {
+        // 정수형 변수 선언과 초기화
+        int number = 10;
+
+        // 실수형 변수 선언과 초기화
+        double decimalNumber = 3.14;
+
+        // 문자열 변수 선언과 초기화
+        String message = "Hello, Java!";
+
+        // boolean 변수 선언과 초기화
+        boolean isTrue = true;
+
+        // 변수 출력
+        System.out.println("정수형 변수: " + number);
+        System.out.println("실수형 변수: " + decimalNumber);
+        System.out.println("문자열 변수: " + message);
+        System.out.println("boolean 변수: " + isTrue);
+    }
+}
