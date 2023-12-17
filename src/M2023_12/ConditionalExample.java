4a5c007153e9b32245143cf2691129ba8b70238a
YoungHyoKim90 <younghyo1101@gmail.com>
2023-12-18 00:16:04 +0900

    ConditionalExample

---
+++ src/M2023_12/ConditionalExample.java
@@ -0,0 +1,17 @@
+package M2023_12;
+
+public class ConditionalExample {
+    public static void main(String[] args) {
+        int number = 10;
+
+        // if 조건문
+        if (number > 0) {
+            System.out.println("양수입니다.");
+        } else if (number < 0) {
+            System.out.println("음수입니다.");
+        } else {
+            System.out.println("0입니다.");
+        }
+    }
+}
+
