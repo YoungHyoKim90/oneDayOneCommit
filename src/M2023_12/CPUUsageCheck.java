0481e628e4cd58c448c23c99b5eb8bf901681ecf
YoungHyoKim90 <younghyo1101@gmail.com>
2023-12-23 23:38:39 +0900

    CPUUsageCheck

---
+++ src/M2023_12/CPUUsageCheck.java
@@ -0,0 +1,14 @@
+package M2023_12;
+
+import java.lang.management.ManagementFactory;
+//import com.sun.management.OperatingSystemMXBean;
+
+//public class CPUUsageCheck {
+//    public static void main(String[] args) {
+//        OperatingSystemMXBean osBean = (com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+//
+//        // CPU 사용률 확인
+//        double cpuUsage = osBean.getSystemCpuLoad();
+//        System.out.println("현재 CPU 사용량: " + (cpuUsage * 100) + "%");
+//    }
+//}
