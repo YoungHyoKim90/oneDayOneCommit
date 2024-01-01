35740e1ad687d594a10fbe752b0e45bdad255e22
YoungHyoKim90 <younghyo1101@gmail.com>
2024-01-01 18:42:54 +0900

    D_0101OppositeCondition

---
+++ src/M2024_01/D_0101OppositeCondition.java
@@ -0,0 +1,15 @@
+package M2024_01;
+
+public class D_0101OppositeCondition {
+    public static void main(String[] args) {
+        boolean condition = true;
+        
+        // 조건의 반대를 검사
+        if (!condition) {
+            System.out.println("조건이 거짓입니다.");
+        } else {
+            System.out.println("조건이 참입니다.");
+        }
+    }
+}
+
