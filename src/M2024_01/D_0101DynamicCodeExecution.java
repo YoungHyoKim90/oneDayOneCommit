c1bddb5fe3f89f2a3fbbd3b0c36c3ca5349c70d7
YoungHyoKim90 <younghyo1101@gmail.com>
2024-01-01 18:40:18 +0900

    D_0101DynamicCodeExecution

---
+++ src/M2024_01/D_0101DynamicCodeExecution.java
@@ -0,0 +1,15 @@
+package M2024_01;
+
+public class D_0101DynamicCodeExecution {
+    public static void main(String[] args) {
+        boolean condition = true;
+        
+        // 조건에 따라 동적으로 코드 실행
+        if (condition) {
+            System.out.println("조건이 참입니다.");
+        } else {
+            System.out.println("조건이 거짓입니다.");
+        }
+    }
+}
+
