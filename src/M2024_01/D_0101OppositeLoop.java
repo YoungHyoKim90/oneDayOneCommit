623672e8885c6b02b2299d7518647978d1492762
YoungHyoKim90 <younghyo1101@gmail.com>
2024-01-01 18:44:02 +0900

    D_0101OppositeLoop

---
+++ src/M2024_01/D_0101OppositeLoop.java
@@ -0,0 +1,11 @@
+package M2024_01;
+
+public class D_0101OppositeLoop {
+    public static void main(String[] args) {
+        // 10부터 1까지 역순으로 반복
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
+
