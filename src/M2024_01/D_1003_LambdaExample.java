7ad73307dcbfc15a7fb3d39ac744ef6f4e91f943
YoungHyoKim90 <younghyo1101@gmail.com>
2024-01-03 00:22:54 +0900

    D_1003_LambdaExample

---
+++ src/M2024_01/D_1003_LambdaExample.java
@@ -0,0 +1,15 @@
+package M2024_01;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class D_1003_LambdaExample {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+        // 람다식으로 제곱 연산
+        numbers.forEach(n -> System.out.print(n * n + " "));
+        // 출력: 1 4 9 16 25
+    }
+}
+
