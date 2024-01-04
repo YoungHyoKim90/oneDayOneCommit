948f77b2a21a5016150c9ee930a7a4c97b92cc1f
YoungHyoKim90 <younghyo1101@gmail.com>
2024-01-05 00:13:53 +0900

    D_0105_DefaultMethodExample

---
+++ src/M2024_01/D_0105_DefaultMethodExample.java
@@ -0,0 +1,19 @@
+package M2024_01;
+
+//interface D_0105_Greeting {
+//    default void sayHello() {
+//        System.out.println("Hello!");
+//    }
+//}
+//
+//class D_0105_GreetingImpl implements D_0105_Greeting {
+//    // 디폴트 메소드를 오버라이딩하지 않고 사용
+//}
+//
+//public class D_0105_DefaultMethodExample {
+//    public static void main(String[] args) {
+//        GreetingImpl greeting = new GreetingImpl();
+//        greeting.sayHello(); // 출력: Hello!
+//    }
+//}
+
