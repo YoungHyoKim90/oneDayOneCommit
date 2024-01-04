d4592eff3dab2dc025ea2ebc42990243f726583b
YoungHyoKim90 <younghyo1101@gmail.com>
2024-01-05 00:30:44 +0900

    D_0105_LambdaExample

--- src/M2024_01/D_0105_LambdaExample.java
+++ src/M2024_01/D_0105_LambdaExample.java
@@ -4,7 +4,7 @@ interface Calculator {
     int calculate(int x, int y);
 }
 
-public class D_0195_LambdaExample {
+public class D_0105_LambdaExample {
     public static void main(String[] args) {
         Calculator addition = (x, y) -> x + y;
         System.out.println(addition.calculate(5, 3)); // 출력: 8
