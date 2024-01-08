10d8beb011dfeec49e9aa4e71149e3047b35b767
YoungHyoKim90 <younghyo1101@gmail.com>
2024-01-08 23:31:26 +0900

    MyAnnotationProcessor

---
+++ src/M2024_01/MyAnnotationProcessor.java
@@ -0,0 +1,21 @@
+package M2024_01;
+
+//import javax.annotation.processing.*;
+//import javax.lang.model.SourceVersion;
+//import javax.lang.model.element.Element;
+//import javax.lang.model.element.TypeElement;
+//import java.util.Set;
+//
+//@SupportedAnnotationTypes("MyAnnotation") // 처리할 어노테이션 지정
+//@SupportedSourceVersion(SourceVersion.RELEASE_6)
+//public class MyAnnotationProcessor extends AbstractProcessor {
+//    @Override
+//    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+//        for (Element elem : roundEnv.getElementsAnnotatedWith(MyAnnotation.class)) {
+//            System.out.println("Found annotated element: " + elem.getSimpleName());
+//        }
+//        return true;
+//    }
+//}
+
+
