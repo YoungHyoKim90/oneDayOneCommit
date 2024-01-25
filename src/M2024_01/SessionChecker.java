39dd9c922881a89372cca96bcb0939c42d108aa7
YoungHyoKim90 <younghyo1101@gmail.com>
2024-01-25 20:28:02 +0900

    SessionChecker

---
+++ src/M2024_01/SessionChecker.java
@@ -0,0 +1,14 @@
+package M2024_01;
+
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpSession;
+//
+//public class SessionChecker {
+//
+//    public static boolean isUserLoggedIn(HttpServletRequest request) {
+//        HttpSession session = request.getSession(false); // false를 전달하여 세션이 없으면 null 반환
+//
+//        return (session != null && session.getAttribute("username") != null);
+//    }
+//}
+
