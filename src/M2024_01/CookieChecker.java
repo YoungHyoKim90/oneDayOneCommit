f30fab287755d916936365c720537314cc70d6ed
YoungHyoKim90 <younghyo1101@gmail.com>
2024-01-25 20:28:34 +0900

    CookieChecker

---
+++ src/M2024_01/CookieChecker.java
@@ -0,0 +1,21 @@
+package M2024_01;
+
+//import javax.servlet.http.Cookie;
+//import javax.servlet.http.HttpServletRequest;
+//
+//public class CookieChecker {
+//
+//    public static String getCookieValue(HttpServletRequest request, String cookieName) {
+//        Cookie[] cookies = request.getCookies();
+//
+//        if (cookies != null) {
+//            for (Cookie cookie : cookies) {
+//                if (cookie.getName().equals(cookieName)) {
+//                    return cookie.getValue();
+//                }
+//            }
+//        }
+//
+//        return null;
+//    }
+//}
