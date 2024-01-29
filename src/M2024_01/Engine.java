d11dc5ccca753fb4e9402c59ae76b112a34254a0
YoungHyoKim90 <younghyo1101@gmail.com>
2024-01-30 00:10:22 +0900

    Engine

---
+++ src/M2024_01/Engine.java
@@ -0,0 +1,41 @@
+//package M2024_01;
+//
+////Engine 클래스
+//public class Engine {
+// public void start() {
+//     System.out.println("Engine started");
+// }
+//
+// public void stop() {
+//     System.out.println("Engine stopped");
+// }
+//}
+//
+////Car 클래스
+//public class Car2 {
+// private Engine engine;
+//
+// public Car2() {
+//     this.engine = new Engine();
+// }
+//
+// public void start() {
+//     engine.start();
+//     System.out.println("Car started");
+// }
+//
+// public void stop() {
+//     engine.stop();
+//     System.out.println("Car stopped");
+// }
+//}
+//
+////메인 클래스
+//public class Main2 {
+// public static void main(String[] args) {
+//     Car2 car = new Car2();
+//     car.start();
+//     car.stop();
+// }
+//}
+//
