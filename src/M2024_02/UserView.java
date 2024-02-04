7f5ba3f7008c8d49dbfa377fc624898c3d75f5e4
YoungHyoKim90 <younghyo1101@gmail.com>
2024-02-05 00:10:48 +0900

    UserView

---
+++ src/M2024_02/UserView.java
@@ -0,0 +1,10 @@
+package M2024_02;
+
+public class UserView {
+    public void printUserDetails(String username, String email) {
+        System.out.println("User Details:");
+        System.out.println("Username: " + username);
+        System.out.println("Email: " + email);
+    }
+}
+
