4ad99c5c47ffc21a0e258abc453f6ade43d52565
YoungHyoKim90 <younghyo1101@gmail.com>
2024-02-12 01:28:24 +0900

    ClassFileDecompiler

--- src/M2024_02/ClassFileDecompiler.java
+++ src/M2024_02/ClassFileDecompiler.java
@@ -11,6 +11,7 @@ public class ClassFileDecompiler {
         decompileClassFile(classFilePath);
     }
 
+    
     public static void decompileClassFile(String classFilePath) {
         try {
             Process process = Runtime.getRuntime().exec("java -jar cfr.jar " + classFilePath); // CFR 실행
@@ -22,6 +23,7 @@ public class ClassFileDecompiler {
                 System.out.println(line); // 디컴파일된 Java 코드 출력
             }
 
+            
             process.waitFor(); // 프로세스 완료 대기
             inputStream.close();
             reader.close();
