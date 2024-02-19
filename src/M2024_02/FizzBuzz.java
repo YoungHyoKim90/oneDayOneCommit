d51e6f3e9b99d02d11637dbea839098ff607d082
YoungHyoKim90 <younghyo1101@gmail.com>
2024-02-20 00:43:13 +0900

    FizzBuzz

---
+++ src/M2024_02/FizzBuzz.java
@@ -0,0 +1,19 @@
+package M2024_02;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            String output = "";
+            if (i % 3 == 0) {
+                output += "Fizz";
+            }
+            if (i % 5 == 0) {
+                output += "Buzz";
+            }
+            if (output.isEmpty()) {
+                output = Integer.toString(i);
+            }
+            System.out.println(output);
+        }
+    }
+}
