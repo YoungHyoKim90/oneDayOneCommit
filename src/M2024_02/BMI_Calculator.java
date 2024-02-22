e34b234de61c35758b7ba73fe0df425e709b9d99
YoungHyoKim90 <younghyo1101@gmail.com>
2024-02-23 00:03:54 +0900

    BMI_Calculator

--- src/M2024_02/BMI_Calculator.java
+++ src/M2024_02/BMI_Calculator.java
@@ -6,16 +6,20 @@ public class BMI_Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        
         // 사용자로부터 키와 몸무게 입력 받기
         System.out.print("키를 입력하세요 (미터 단위): ");
         double height = scanner.nextDouble();
 
+        
         System.out.print("몸무게를 입력하세요 (킬로그램 단위): ");
         double weight = scanner.nextDouble();
 
+        
         // BMI 계산
         double bmi = calculateBMI(height, weight);
 
+        
         // BMI 결과 출력
         System.out.println("당신의 BMI 지수는 " + bmi + "입니다.");
         System.out.println("BMI 결과 해석:");
