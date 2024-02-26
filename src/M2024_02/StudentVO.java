53107f23a16b7fd2cb1978ff51204f8910f080f7
YoungHyoKim90 <younghyo1101@gmail.com>
2024-02-26 23:47:15 +0900

    StudentVO

---
+++ src/M2024_02/StudentVO.java
@@ -0,0 +1,60 @@
+package M2024_02;
+
+public class StudentVO {
+    private int id;
+    private String name;
+    private int age;
+    private String major;
+
+    // 생성자
+    public StudentVO(int id, String name, int age, String major) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.major = major;
+    }
+
+    // Getter 및 Setter 메서드
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentVO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", major='" + major + '\'' +
+                '}';
+    }
+}
+
