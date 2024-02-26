ff24970241652481ff8006b0b94caaeec831bbac
YoungHyoKim90 <younghyo1101@gmail.com>
2024-02-27 00:05:41 +0900

    StudentDTO

---
+++ src/M2024_02/StudentDTO.java
@@ -0,0 +1,63 @@
+package M2024_02;
+
+public class StudentDTO {
+    private int id;
+    private String name;
+    private int age;
+    private String major;
+
+    // 생성자
+    public StudentDTO() {
+    }
+
+    public StudentDTO(int id, String name, int age, String major) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.major = major;
+    }
+
+    // Getter 및 Setter 메서드
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", major='" + major + '\'' +
+                '}';
+    }
+}
+
--- src/M2024_02/a.java
+++
@@ -1,5 +0,0 @@
-package M2024_02;
-
-public class a {
-
-}
