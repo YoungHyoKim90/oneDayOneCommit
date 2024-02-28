1a279b768f89fff7179388884173a0d587c0ca4d
YoungHyoKim90 <younghyo1101@gmail.com>
2024-02-29 00:26:18 +0900

    EconomicIndexCalculator

--- src/M2024_02/EconomicIndexCalculator.java
+++ src/M2024_02/EconomicIndexCalculator.java
@@ -6,6 +6,7 @@ public class EconomicIndexCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        
         // 각 요소의 가중치 입력 받기
         System.out.println("경제 지표 요소의 가중치를 입력하세요.");
         System.out.print("실업률의 가중치: ");
@@ -15,6 +16,7 @@ public class EconomicIndexCalculator {
         System.out.print("경기 순환 지수의 가중치: ");
         double businessCycleIndexWeight = scanner.nextDouble();
 
+        
         // 각 요소의 예측치 입력 받기
         System.out.println("경제 지표 요소의 예측치를 입력하세요.");
         System.out.print("예상 실업률: ");
