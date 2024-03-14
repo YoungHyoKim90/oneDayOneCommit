f0162a23aad39824d82db935bee42350c8858cbb
YoungHyoKim90 <younghyo1101@gmail.com>
2024-03-15 00:01:30 +0900

    Service

--- src/M2024_03/Service.java
+++ src/M2024_03/Service.java
@@ -3,22 +3,18 @@ package M2024_03;
 
 public class Service {
 	
-	
 	@PrintAnnotation
 	private String name;
-	
 	
 	@PrintAnnotation()
 	public void method1() {
 		System.out.println("메서드1 입니다.");
 	}
 	
-	
 	@PrintAnnotation("%")
 	public void method2() {
 		System.out.println("메서드2 입니다.");
 	}
-	
 	
 	@PrintAnnotation(value = "#", count = 25)
 	public void method3() {
