94f51a7110ae07da2eaa9e257cb8c4f457da1993
YoungHyoKim90 <younghyo1101@gmail.com>
2024-03-18 00:07:45 +0900

    Sum

---
+++ src/M2024_03/Sum.java
@@ -0,0 +1,11 @@
+package M2024_03;
+
+public class Sum {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+        System.out.println("두 정수의 합: " + sum);
+    }
+}
+
