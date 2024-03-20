785d1922398c8660a5af2cc26c3242bb2d31f549
YoungHyoKim90 <younghyo1101@gmail.com>
2024-03-20 19:55:01 +0900

    LottoGenerator

--- src/M2024_03/LottoGenerator.java
+++ src/M2024_03/LottoGenerator.java
@@ -10,14 +10,11 @@ public class LottoGenerator {
         for (int i = 1; i <= 46; i++) {
             numbers.add(i);
         }
-
         Collections.shuffle(numbers);
-
         System.out.println("로또 번호를 추첨합니다.");
         System.out.print("추첨된 번호: ");
         for (int i = 0; i < 6; i++) {
             System.out.print(numbers.get(i) + " ");
-            
         }
     }
 }
