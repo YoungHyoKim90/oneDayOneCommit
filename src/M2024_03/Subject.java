2395157f5f13eb0648d0bc6413d756dce2c85040
YoungHyoKim90 <younghyo1101@gmail.com>
2024-03-28 00:04:03 +0900

    Subject

--- src/M2024_03/Subject.java
+++ src/M2024_03/Subject.java
@@ -28,3 +28,61 @@ public class Subject {
     }
 }
 
+/*
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subject {
+    private List<Observer> observers = new ArrayList<>();
+    private int state;
+    
+    public int getState() {
+        return state;
+    }
+    
+    public void setState(int state) {
+        this.state = state;
+        notifyAllObservers();
+    }
+    
+    public void attach(Observer observer) {
+        observers.add(observer);
+    }
+    
+    public void notifyAllObservers() {
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }
+}
+
+public abstract class Observer {
+    protected Subject subject;
+    public abstract void update();
+}
+
+public class BinaryObserver extends Observer {
+    public BinaryObserver(Subject subject) {
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+    
+    @Override
+    public void update() {
+        System.out.println("Binary String: " + Integer.toBinaryString(subject.getState()));
+    }
+}
+
+public class OctalObserver extends Observer {
+    public OctalObserver(Subject subject) {
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+    
+    @Override
+    public void update() {
+        System.out.println("Octal String: " + Integer.toOctalString(subject.getState()));
+    }
+}
+
+*/
