44a760e9d5f55a63361996def88c187120f7942e
YoungHyoKim90 <younghyo1101@gmail.com>
2024-04-02 16:43:40 +0900

    File

---
+++ src/M2024_04/asd.java
@@ -0,0 +1,18 @@
+package M2024_04;
+
+public class 설명 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("File: 파일과 디렉토리에 대한 경로와 속성을 다루는 클래스입니다. (java.io.File)\r\n" + 
+				"FileInputStream / FileOutputStream: 파일로부터 데이터를 읽거나 쓰기 위한 클래스입니다. (java.io.FileInputStream, java.io.FileOutputStream)\r\n" + 
+				"BufferedReader / BufferedWriter: 버퍼링된 입력 스트림 및 출력 스트림을 생성하기 위한 클래스입니다. (java.io.BufferedReader, java.io.BufferedWriter)\r\n" + 
+				"FileReader / FileWriter: 파일로부터 문자를 읽거나 쓰기 위한 클래스입니다. (java.io.FileReader, java.io.FileWriter)\r\n" + 
+				"RandomAccessFile: 파일 내에서 임의의 위치로 이동하여 데이터를 읽거나 쓸 수 있는 클래스입니다. (java.io.RandomAccessFile)\r\n" + 
+				"Path: 파일 시스템의 경로를 나타내는 클래스입니다. (java.nio.file.Path)\r\n" + 
+				"Files: 파일과 디렉토리를 다루는 메서드를 제공하는 유틸리티 클래스입니다. (java.nio.file.Files)"
+				);
+		
+	}
+	
+}
