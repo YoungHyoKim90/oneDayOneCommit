ddb57029d3e0fb85893aa0fa3ee949220b415740
YoungHyoKim90 <younghyo1101@gmail.com>
2024-04-04 12:54:07 +0900

    basicType

---
+++ src/M2024_04/basicType.java
@@ -0,0 +1,19 @@
+package M2024_04;
+
+public class basicType {
+    public static void main(String[] args) {
+          /* 레퍼런스 데이터 타입 */
+    Integer i = 1;
+    Float f = 1.1f;
+    Double d = 1.2;
+    Boolean b = true;
+    Character c = 'a';
+    String s = "Hello World";
+    System. out.println( i);
+    System. out.println( f);
+    System. out.println( d);
+    System. out.println( b);
+    System. out.println( c);
+    System. out.println(s );
+   }
+}
