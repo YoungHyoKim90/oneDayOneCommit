025a7032afff3b89c30f7db1f73b35c6c04b349c
YoungHyoKim90 <younghyo1101@gmail.com>
2024-04-04 16:19:14 +0900

    배열

---
+++ src/M2024_04/array.java
@@ -0,0 +1,9 @@
+package M2024_04;
+
+public class array {
+    public static void main(String[] args) {
+          /* 배열 선언 */
+    int[] arr = {1,2,3,4};
+    System. out.println(arr .getClass());
+   }
+}
