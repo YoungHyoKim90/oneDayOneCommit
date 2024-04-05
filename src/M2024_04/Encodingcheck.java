f6f1d9623f45655b75d3c1e2aafddf982329c73f
YoungHyoKim90 <younghyo1101@gmail.com>
2024-04-05 13:11:36 +0900

    Encodingcheck

--- src/M2024_04/Encodingcheck.java
+++ src/M2024_04/Encodingcheck.java
@@ -4,10 +4,12 @@ import java.nio.charset.Charset;
 
 public class Encodingcheck {
     public static void main(String[] args) {
+    	
         // 현재 클래스 파일의 인코딩 종류 확인
         String classFileEncoding = System.getProperty("file.encoding");
         System.out.println("현재 클래스 파일의 인코딩 종류: " + classFileEncoding);
 
+        
         // 출력 프로그램이 사용하고 있는 인코딩 종류 확인
         String defaultCharset = Charset.defaultCharset().name();
         System.out.println("출력 프로그램이 사용하고 있는 인코딩 종류: " + defaultCharset);
