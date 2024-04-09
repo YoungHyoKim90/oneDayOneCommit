03b4967612160c6634e2b717e8da0e567e89875e
YoungHyoKim90 <younghyo1101@gmail.com>
2024-04-09 09:54:10 +0900

    MinimumSpanningTree_Kruskal

---
+++ src/M2024_04/MinimumSpanningTree_Kruskal.java
@@ -0,0 +1,114 @@
+package M2024_04;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+class MinimumSpanningTree_Kruskal {
+
+    static class Edge implements Comparable<Edge> {
+        int x, y, w;
+
+        public Edge(int x, int y, int w) {
+            this.x = x;
+            this.y = y;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Edge other) {
+            return Integer.compare(this.w, other.w);
+        }
+    }
+
+    static final int MAXV = 100005;
+    static final int MAXE = 500005;
+
+    static Edge[] edge = new Edge[MAXE];
+    static Edge[] tmp = new Edge[MAXE];
+
+    static int[] disjoint_set = new int[MAXV];
+
+    static int find_root(int x) {
+        if (disjoint_set[x] == x)
+            return x;
+
+        disjoint_set[x] = find_root(disjoint_set[x]);
+
+        return disjoint_set[x];
+    }
+
+    static void mergeSort(int s, int e) {
+        if (s < e) {
+            int m = (s + e) / 2;
+            mergeSort(s, m);
+            mergeSort(m + 1, e);
+            int left = s, right = m + 1, pointer = s;
+            while (left <= m && right <= e) {
+                if (edge[left].w <= edge[right].w) {
+                    tmp[pointer++] = edge[left++];
+                } else {
+                    tmp[pointer++] = edge[right++];
+                }
+            }
+            while (left <= m)
+                tmp[pointer++] = edge[left++];
+            while (right <= e)
+                tmp[pointer++] = edge[right++];
+            for (int i = s; i <= e; i++) {
+                edge[i] = tmp[i];
+            }
+        }
+    }
+
+    static void kruskal(int V, int E) {
+        int mstTotalWeight = 0;
+
+        for (int i = 0; i < E; i++) {
+
+            int groupX = find_root(edge[i].x);
+            int groupY = find_root(edge[i].y);
+
+            if (groupX == groupY) {
+                continue;
+            }
+
+            mstTotalWeight += edge[i].w;
+
+            disjoint_set[groupY] = groupX;
+            System.out.println("Union " + groupX + " - " + groupY);
+        }
+
+        System.out.println(mstTotalWeight);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int TC = scanner.nextInt();
+        for (int t = 0; t < TC; t++) {
+
+            int V = scanner.nextInt();
+            int E = scanner.nextInt();
+
+            for (int i = 1; i <= V; i++) {
+                System.out.println("Draw vertex " + i);
+                disjoint_set[i] = i;
+            }
+
+            for (int i = 0; i < E; i++) {
+                int x = scanner.nextInt();
+                int y = scanner.nextInt();
+                int w = scanner.nextInt();
+                edge[i] = new Edge(x, y, w);
+            }
+
+            Arrays.sort(edge, 0, E);
+
+            System.out.print("#" + (t + 1) + " ");
+            kruskal(V, E);
+        }
+
+        scanner.close();
+    }
+}
