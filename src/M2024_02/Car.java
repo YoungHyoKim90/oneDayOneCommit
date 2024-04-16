fe8d5e7a6ee8ab2808fe087bc10c1400ed2229a8
YoungHyoKim90 <younghyo1101@gmail.com>
2024-04-16 19:34:07 +0900

    Car

--- src/M2024_02/Car.java
+++ src/M2024_02/Car.java
@@ -1,35 +1,33 @@
 package M2024_02;
 
 public class Car {
-    private final String make;
-    private final String model;
-    private final int year;
+	private final String make;
+	private final String model;
+	private final int year;
 
-    private Car(Builder builder) {
-        this.make = builder.make;
-        this.model = builder.model;
-        this.year = builder.year;
-    }
+	private Car(Builder builder) {
+		this.make = builder.make;
+		this.model = builder.model;
+		this.year = builder.year;
+	}
 
-    public static class Builder {
-        private final String make;
-        private final String model;
-        private int year;
+	public static class Builder {
+		private final String make;
+		private final String model;
+		private int year;
 
-        public Builder(String make, String model) {
-            this.make = make;
-            this.model = model;
-        }
+		public Builder(String make, String model) {
+			this.make = make;
+			this.model = model;
+		}
 
-        public Builder year(int year) {
-            this.year = year;
-            return this;
-        }
+		public Builder year(int year) {
+			this.year = year;
+			return this;
+		}
 
-        public Car build() {
-            return new Car(this);
-        }
-    }
+		public Car build() {
+			return new Car(this);
+		}
+	}
 }
-
-
