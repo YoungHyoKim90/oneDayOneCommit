d076bb6aa1a7f086a2704dcedf550d52c7a2cc08
YoungHyoKim90 <younghyo1101@gmail.com>
2024-05-05 22:17:25 +0900

    welcome05

--- src/M2024_05/welcome05.java
+++ src/M2024_05/welcome05.java
@@ -2,13 +2,15 @@ package M2024_05;
 
 public class welcome05 {
 	
-	public static void main(String[] args) {
+	public static void main(String args) {
 		System.out.println("벌써5월이야!");
 		Thread.activeCount();
 		System.out.println("벌써5월이야!");
 		boolean condition = false;
 		if (condition) {
-			
+			String[] args2 = new String[args.length() + 1];
+			System.arraycopy(args, 0, args2, 0, args.length());
+			args2[args.length()] = args;
 			
 		}
 	}
