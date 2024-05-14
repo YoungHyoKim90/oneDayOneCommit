6df58c33e72ee2b55c516237ed5c20b46bac91b4
YoungHyoKim90 <younghyo1101@gmail.com>
2024-05-15 00:09:48 +0900

    absBlack

--- src/M2024_05/absBlack.java
+++ src/M2024_05/absBlack.java
@@ -2,4 +2,22 @@ package M2024_05;
 
 public class absBlack {
 
+	
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+		
+		System.out.println("a+b : " + a+b);
+		
+		System.out.println("hi : " + hi.class.getModifiers());
+		
+		
+		
+	}
+	
+	class hi{
+		int a = 10;
+		int b = 20;
+		int sum = a+b;
+	}
 }
