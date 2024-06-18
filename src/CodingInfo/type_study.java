640014977f812ea9b6becf164d6f594f86c34eed
YoungHyoKim90 <younghyo1101@gmail.com>
2024-06-18 14:41:55 +0900

    type_study

---
+++ src/CodingInfo/type_study.java
@@ -0,0 +1,19 @@
+package CodingInfo;
+
+public class type_study {
+
+	public static void main(String[] args) {
+		
+		char var1 = 'A';
+		char var2 = 'B';
+		char var3 = '가';
+		char var4 = '각';
+		
+		System.out.println("var1 = " + var1);
+		System.out.println("var2 = " + var2);
+		System.out.println("var3 = " + var3);
+		System.out.println("var4 = " + var4);
+		
+	}
+	
+}
