fb8de398644a0316b2215f139652f40b72737ee3
YoungHyoKim90 <younghyo1101@gmail.com>
2024-06-25 14:48:26 +0900

    TypeType

---
+++ src/M2024_06/TypeType.java
@@ -0,0 +1,16 @@
+package M2024_06;
+
+public class TypeType {
+
+	public static void main(String[] args) {
+		//기본 타입의 값 비교와 문자열 비교
+		int x = 5;
+		boolean result = (x == 5);
+		boolean result2 = (x == 4);
+		
+		System.out.println("result = "+ result);
+		System.out.println("result2 = "+ result2);
+		
+	}
+	
+}
