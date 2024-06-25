3006ef3d30c8b48ed12dc0b1c2381e80740bc8f7
YoungHyoKim90 <younghyo1101@gmail.com>
2024-06-26 08:47:03 +0900

    booleanTypeTest

---
+++ src/M2024_06/booleanTypeTest.java
@@ -0,0 +1,22 @@
+package M2024_06;
+
+public class booleanTypeTest {
+	public static void main(String[] args) {
+		
+		//논리 연산자 boolean 타입만 사용이 가능하다. true, false
+		
+		int a = 10;
+		int b = 20;
+		int c = 30;
+		
+		boolean trueAndFalse = false;
+		
+		System.out.println( trueAndFalse((a+b)>c));
+		
+	}
+
+	private static char[] trueAndFalse(boolean b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
