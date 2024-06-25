325a58df0e6fe0c5f93c5a764b5df9f86f93ad60
YoungHyoKim90 <younghyo1101@gmail.com>
2024-06-25 09:25:39 +0900

    parseChange

---
+++ src/M2024_06/parseChange.java
@@ -0,0 +1,40 @@
+package M2024_06;
+
+public class parseChange {
+	public static void main(String[] args) {
+		String strValue = "10";
+		String strValue2 = "200";
+		String strValue3= "3000";
+		String strValue4 = "10.999";
+		String strValue5 = "20.888";
+		String strValue6 = "true";
+		String strValue7 = "false";
+		
+		byte byteValue = Byte.parseByte(strValue);
+		System.out.println("byteValue =" + byteValue);
+		
+		short shortValue = Short.parseShort(strValue);
+		System.out.println("shortValue =" + shortValue);
+		
+		int intValue = Integer.parseInt(strValue);
+		System.out.println("intValue =" + intValue);
+
+		long longValue = Long.parseLong(strValue);
+		System.out.println("longValue =" + longValue);
+		
+		float floatValue = Float.parseFloat(strValue);
+		System.out.println("floatValue =" + floatValue );
+		
+		double doubleValue = Double.parseDouble(strValue);
+		System.out.println("doubleValue =" + doubleValue);
+		
+		boolean booleanValue = Boolean.parseBoolean(strValue6);
+		System.out.println("booleanValue =" + booleanValue);
+		
+		booleanValue = Boolean.parseBoolean(strValue7);
+		System.out.println("booleanValue =" + booleanValue);
+		
+		
+		
+	}
+}
