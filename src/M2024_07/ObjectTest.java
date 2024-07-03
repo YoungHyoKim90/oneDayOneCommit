34e0b74b781444b98340d1dd72288fd95b46f0a7
YoungHyoKim90 <younghyo1101@gmail.com>
2024-07-03 21:13:01 +0900

    ObjectTest

---
+++ src/M2024_07/ObjectTest.java
@@ -0,0 +1,12 @@
+package M2024_07;
+
+/**
+ * 클래스, 객체, new 연산자, 클래스 변수, 인스턴스, 클래스 멤버
+ * 
+ * @author PigYoung90 객체 (Object) 물리적으로 존재하거나 추상적으로 생각할 수 있는 것 중에서 자신의 속성을 가지며
+ *         식별 가능한 것 속성 (필드(field)) + 동작(메소드(method))로 구성
+ * 
+ */
+public class ObjectTest {
+
+}
