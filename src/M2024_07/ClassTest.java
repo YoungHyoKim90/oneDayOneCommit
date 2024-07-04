b90f8aa48b68fab68a1ebc19dea22bd8ef27a8d4
YoungHyoKim90 <younghyo1101@gmail.com>
2024-07-04 13:09:48 +0900

    ClassTest

---
+++ src/M2024_07/ClassTest.java
@@ -0,0 +1,13 @@
+package M2024_07;
+
+public class ClassTest {
+
+	public static void main(String[] args) {
+		
+		ClassTest c1 = new ClassTest();
+		System.out.println("c1 변수가 ClassTest 객체를 참조합니다.");
+		
+	}
+	
+}
+
