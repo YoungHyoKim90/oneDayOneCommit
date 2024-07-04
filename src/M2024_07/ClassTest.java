bd5fa07fa90e2b3d2248831dd09414b734c18b28
YoungHyoKim90 <younghyo1101@gmail.com>
2024-07-04 15:58:52 +0900

    ClassTest

--- src/M2024_07/ClassTest.java
+++ src/M2024_07/ClassTest.java
@@ -2,12 +2,23 @@ package M2024_07;
 
 public class ClassTest {
 
+	
 	public static void main(String[] args) {
+		
 		
 		ClassTest c1 = new ClassTest();
 		System.out.println("c1 변수가 ClassTest 객체를 참조합니다.");
 		
+		
+		ClassTest c2 = new ClassTest();
+		System.out.println("c2 변수가 ClassTest 객체를 참조합니다.");
 	}
 	
 }
 
+/* 클래스의 두 용도
+라이브러리(API : Application Program Interface) 클래스
+객체 생성 및 메소드 제공 역할  - Student.java
+실행 클래스
+ main() 메소드 제공 역할 - StudentExample.java
+*/
