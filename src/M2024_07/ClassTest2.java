09348ca813eeccd857757ed7db5bc543c954e541
YoungHyoKim90 <younghyo1101@gmail.com>
2024-07-07 12:57:46 +0900

    ClassTest2

--- src/M2024_07/ClassTest2.java
+++ src/M2024_07/ClassTest2.java
@@ -14,19 +14,18 @@ public class ClassTest2 {
 	 * 필드 선언 클래스 중괄호 블록 어디서든 존재 가능 생성자와 메소드 중괄호 블록 내부에는 선언 불가 변수와 선언 형태 유사하나 변수 아님에
 	 * 주의
 	 */
-	
-	class hundai{
-		String company ="현대자동차";
-		String model ="그랜저";
+
+	class hundai {
+		String company = "현대자동차";
+		String model = "그랜저";
 		int maxSpeed = 300;
 		int productionYear;
 		int currentSpeed;
 		boolean engineStart;
-		
-		
-		//초기값은 주어질 수도, 생략할 수도 있음
-		//초기값 지정되지 않은 필드는 객체 생성 시 자동으로 기본 초기값 설정
+
+		// 초기값은 주어질 수도, 생략할 수도 있음
+		// 초기값 지정되지 않은 필드는 객체 생성 시 자동으로 기본 초기값 설정
 
 	}
-	
+
 }
