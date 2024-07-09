92e1ae5b6c4587d42749c2094407d11f6d5144ca
YoungHyoKim90 <younghyo1101@gmail.com>
2024-07-09 17:27:29 +0900

    overloadingTest

---
+++ src/M2024_07/overloadingTest.java
@@ -0,0 +1,20 @@
+package M2024_07;
+
+/*
+ * 생성자 오버로딩 (overloading)
+매개 변수를 달리하는 생성자 여러 개 선언
+외부에서 제공되는 다양한 데이터를 사용하여 객체 화하기 위해
+
+ */
+public class overloadingTest {
+
+//	class a([int intValue])
+//	{
+//		
+//	}
+//
+//	clss b([int intValue2]) {
+//		
+//	}
+	
+}
