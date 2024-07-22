d78b6c9c68b56059b2f91532ae68cdd08faa031e
YoungHyoKim90 <younghyo1101@gmail.com>
2024-07-22 13:03:43 +0900

    SignatureTest

--- src/M2024_07/SignatureTest.java
+++ src/M2024_07/SignatureTest.java
@@ -68,6 +68,15 @@ public class SignatureTest {
 	public static void main(String[] args) {
 		
 	}
+	
+	int plus(int x, int y)	{
+		int result = x + y ;
+		return result;
+	}
+	double plus (double x, double y) {
+		double result = x + y ;
+		return result;
+	}
 }
 
 
