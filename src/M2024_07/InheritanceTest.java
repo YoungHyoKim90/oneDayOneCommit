15553305be9127d1b72ceb231cbf8f022d8ca6ea
YoungHyoKim90 <younghyo1101@gmail.com>
2024-07-29 12:30:55 +0900

    InheritanceTest

--- src/M2024_07/InheritanceTest.java
+++ src/M2024_07/InheritanceTest.java
@@ -16,10 +16,23 @@ public class InheritanceTest {
 	}
 
 	public class B extends InheritanceTest {
+		//extends를 통하여 
+		
 		String field2;
 
 		void method2() {
 
+		}
+	}
+	
+	public class 부모클래스 {
+		void method3() {
+			
+		}
+	}
+	public class 자식클래스 extends 부모클래스 {
+		void method4() {
+			
 		}
 	}
 }
